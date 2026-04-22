c385afed0f652f4a8912cceae5ccdaf751b42005
hlpriyanka77-rgb <hlpriyanka77@gmail.com>
2026-04-22 16:56:38 +0530

    golden_model.py

---
+++ golden_model.py
@@ -0,0 +1,40 @@
+
+def single_row_distance(char_a, string_b):
+    n = len(string_b)
+
+
+# Initial row (0,1,2,3,...)
+prev_row = list(range(n + 1))
+curr_row = [0] * (n + 1)
+
+# First column
+curr_row[0] = prev_row[0] + 1
+
+# Compute row
+for j in range(1, n + 1):
+    cost = 0 if char_a == string_b[j - 1] else 1
+
+    curr_row[j] = min(
+        prev_row[j] + 1,
+        curr_row[j - 1] + 1,
+        prev_row[j - 1] + cost
+    )
+
+print("Previous Row:", prev_row)
+print("Current Row :", curr_row)
+print("Final Output:", curr_row[n])
+
+return curr_row[n]
+
+
+# Test case
+
+if __name__ == "__main__":   
+char_a = 'K'
+string_b = "SITT"
+
+
+result = single_row_distance(char_a, string_b)
+print("Expected Distance =", result)
+
+

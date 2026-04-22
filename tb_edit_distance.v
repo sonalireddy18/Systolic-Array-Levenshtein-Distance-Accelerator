3c3b296c6706190d30a9fedb1777e10fa97fa2a9
hlpriyanka77-rgb <hlpriyanka77@gmail.com>
2026-04-22 17:03:46 +0530

    tb_edit_distance.v

--- tb_edit_distance.v
+++ tb_edit_distance.v
@@ -12,7 +12,7 @@ reg [7:0] d_in_initial;
 
 wire [7:0] final_distance;
 
-// Instantiate DUT
+
 edit_distance_top #(LENGTH) uut (
 .clk(clk),
 .rst(rst),
@@ -22,31 +22,31 @@ edit_distance_top #(LENGTH) uut (
 .final_distance(final_distance)
 );
 
-// Clock generation
+
 always #5 clk = ~clk;
 
 initial begin
-// Initialize
+
 rst = 1;
 char_a_in = 8'd0;
 string_b = 0;
 d_in_initial = 8'd0;
 
 
-// Apply inputs
-#10;
-char_a_in = "K";                  // character A
-string_b  = {"S","I","T","T"};    // string B
 
-// Release reset
+#10;
+char_a_in = "K";                  
+string_b  = {"S","I","T","T"};    
+
+
 #10 rst = 0;
 
 $display("Starting Test...");
 
-// Wait for pipeline computation
+
 #100;
 
-// Output result
+
 $display("Final Distance Output = %d", final_distance);
 
 $finish;
